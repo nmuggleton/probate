ee85fc2647685cb33aae8a7fe7f59c99e116acf2
Naomi Muggleton <Naomi.muggleton@spi.ox.ac.uk>
2021-03-25 11:03:25 +0000

    Create setup.sh
    Installs tesseract and dependencies on machine

---
+++ 00-installation/setup.sh
@@ -0,0 +1,46 @@
+# 1. Install dependencies
+sudo yum update
+sudo yum install gcc -y
+sudo yum install gcc-c++ -y
+sudo yum install make -y
+sudo yum install autoconf -y
+sudo yum install automake -y
+sudo yum install libtool -y
+sudo yum install libjpeg-devel -y
+sudo yum install libpng-devel -y
+sudo yum install libtiff-devel -y
+sudo yum install zlib-devel -y
+sudo yum install mesa-libGL -y
+sudo yum install kernel-devel -y
+sudo yum install python3 -y
+
+# 2. Compile and install leptonica
+cd ~
+mkdir leptonica
+cd leptonica
+wget http://www.leptonica.org/source/leptonica-1.80.0.tar.gz
+tar -zxvf leptonica-1.80.0.tar.gz
+cd leptonica-1.80.0
+./configure
+make
+sudo make install
+export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig
+
+# 3. Compile and install tesseract
+cd ~
+mkdir tesseract
+cd tesseract
+wget https://github.com/tesseract-ocr/tesseract/archive/4.1.1.tar.gz
+tar -zxvf 4.1.1.tar.gz
+cd tesseract-4.1.1
+./autogen.sh
+./configure
+make
+sudo make install
+sudo ldconfig
+
+# 4. Download trained language data to tessdata
+cd /usr/local/share/tessdata
+sudo wget https://github.com/tesseract-ocr/tessdata/raw/master/eng.traineddata
+# sudo mv -v eng.traineddata /usr/local/share/
+export TESSDATA_PREFIX=/usr/local/share/
